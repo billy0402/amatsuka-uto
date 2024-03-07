2f3bdbad9892d7952631c6a4537892f2cd6fa299
Billy Huang <10646003@ntub.edu.tw>
2024-03-07 11:11:15 +0800

    feat(seo): add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://billy0402.github.io/amatsuka-uto',
+  exclude: ['/contact'],
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  changefreq: 'monthly',
+};
