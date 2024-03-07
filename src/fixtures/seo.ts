7be1112248c87a45df2d13a55b47984453085678
Billy Huang <10646003@ntub.edu.tw>
2024-03-07 11:02:02 +0800

    feat(seo): head add meta tags

---
+++ src/fixtures/seo.ts
@@ -0,0 +1,16 @@
+import { basePath } from '@/fixtures/env';
+
+export const seoTitle = 'Amatsuka Uto | Blanche Ailes';
+
+export const seoDescription = `\
+未熟な天使の女の子。
+FPSとショットガンが大好き。
+本気を出せば翼を使えるらしい。
+実は大が付くほどのママっこ。\
+`;
+
+export const seoUrl = 'https://billy0402.github.io/amatsuka-uto';
+
+export const seoSocialPreviewImage = `${basePath}/images/social-preview.jpg`;
+
+export const seoFavoriteIcon = `${basePath}/favicon.ico`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,47 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import Layout from '@/components/Layout';
-import { basePath } from '@/fixtures/env';
+import {
+  seoDescription,
+  seoFavoriteIcon,
+  seoSocialPreviewImage,
+  seoTitle,
+  seoUrl,
+} from '@/fixtures/seo';
 import '@/styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        <title>Amatsuka Uto | Blanche Ailes</title>
-        <link
-          rel='shortcut icon'
-          type='image/x-icon'
-          href={`${basePath}/favicon.ico`}
+        <title>{seoTitle}</title>
+        <link rel='shortcut icon' type='image/x-icon' href={seoFavoriteIcon} />
+        <link rel='icon' type='image/x-icon' href={seoFavoriteIcon} />
+        <link rel='apple-touch-icon' href={seoFavoriteIcon} />
+        <link rel='bookmark' href={seoFavoriteIcon} />
+
+        {/* Primary Meta Tags */}
+        <title>{seoTitle}</title>
+        <meta name='title' content={seoTitle} />
+        <meta name='description' content={seoDescription} />
+        <meta
+          name='keywords'
+          content='天使うと,あまつかうと,Amatsuka Uto,Uto,Vtuber'
         />
+
+        {/* Open Graph / Facebook */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={seoUrl} />
+        <meta property='og:title' content={seoTitle} />
+        <meta property='og:description' content={seoDescription} />
+        <meta property='og:image' content={seoSocialPreviewImage} />
+
+        {/* Twitter */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content={seoUrl} />
+        <meta property='twitter:title' content={seoTitle} />
+        <meta property='twitter:description' content={seoDescription} />
+        <meta property='twitter:image' content={seoSocialPreviewImage} />
       </Head>
       <Component {...pageProps} />
     </Layout>
